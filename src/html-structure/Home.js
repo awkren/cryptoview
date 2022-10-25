62de183047f7f94145fd84a6cbece88e7c3eccde
Renan Araújo <renan.araujoonline@gmail.com>
2022-10-25 09:22:10 -0300

    finished first part of homepage

--- src/html-structure/Home.js
+++ src/html-structure/Home.js
@@ -48,19 +48,46 @@ const Home = () => {
 
     </div>
 
-    <div id='mid-home-content' className='flex flex-row text-xl ml-[6rem] mr-[6rem]'>
-      <div id='left-text' className='mt-[9rem] ml-[2rem]'>
-        <p className=''>Tudo em um só lugar.</p>
-        <p id='' className='text-5xl font-bold leading-[6rem]'>Nós levamos dados a sério!</p>
-        <p className=' text-5xl font-bold leading-[1rem]'>Uptime de 99%, 24/7.</p>
-        <p className='leading-[6rem]'>Acompanhe suas moedas favoritas 24/7.</p>
+    <div id='mid-home-content' className='flex flex-row text-xl ml-[6rem] mr-[6rem] items-center pt-[2rem]'>
+      <div id='left-text' className='w-[50%]'>
+        <div id='second-text' className=''>
+        <p id='left1' className='text-6xl font-extrabold leading-[6rem]'>Nós levamos dados a sério!</p>
+        <p id='left1' className=' text-6xl font-extrabold leading-[3rem]'>Uptime de 99%, 24/7.</p>
+        </div>
+        <p id='highlight' className='leading-[4rem] text-center underline text-2xl'>Tudo em um só lugar.</p>
+        <p id='left1' className='mt-[1.2rem]'>Acompanhe suas moedas favoritas em tempo real.
+         Nós da CryptoView garantimos aproximadamente 95% de precisão em nossas análises.<br></br>
+         Contamos com um time mundialmente conhecidos e com <em className='font-bold not-italic'>informações secretas</em> que nos
+         ajudam a garantir um retorno posivo para os nossos parceiros!</p>
+        <div id='newsletter-div' className='flex flex-col items-center'>
+          <p className='text-center mt-[3rem]'>Inscreva-se para ser atualizado sempre que tivermos novas informações:</p>
+          <form id='submit-newsletter' className='m-auto'>
+            <input id='input-newsletter' type='mail' placeholder='youremail@something.com' className='w-[30rem] h-[3rem] mt-[2.5px] border-1 rounded-[5px]'>
+            </input>
+            <span id='sub-button'><a href="#"></a></span>
+          </form>
+        </div>
       </div>
-      <div id='right-img'>
+      <div id='right-img' className='w-[50%]'>
         <img src={cryptoHeroTwo} alt='/'/>
       </div>
     </div>
+
+    <div class="scroll-downs">
+        <div class="mousey">
+          <div class="scroller"></div>
+        </div>
+    </div>
+    <br></br>
+    <br></br>
     
-    
+    <div id='second-half-homepage' className='bg-[#f2e8de] text-[#1f1f1f]'>
+      <h1>Teste</h1>
+      <h1>Teste</h1>
+      <h1>Teste</h1>
+      <h1>Teste</h1>
+    </div>
+
   </>)
 }
 
