48541955ef61ea7f76fcde27391bc801ab513bce
Renan Araújo <renan.araujoonline@gmail.com>
2022-11-11 10:11:38 -0300

    experts section responsive

--- src/cryptoview-structure/AboutExperts.tsx
+++ src/cryptoview-structure/AboutExperts.tsx
@@ -11,7 +11,7 @@ const AboutExperts = () => {
         <img className='w-[500px]' src={cryptoThree} alt='/'/>
       </div>
       <div id='about-experts-right' className='m-auto text-center'>
-        <p className='text-[3rem] font-semibold'>We work with the best</p>
+        <p id='title-experts' className='text-[3.5rem] font-bold'>We work with the best</p>
         <p className='text-[1.5rem] w-[95%] m-auto text-justify mt-[1rem] mb-[1rem]'>Our consultors have worked in the biggest banks in the world.
         We understand what brings results, that's why we are willing to give you the best experience and professionals in the market.</p>
         <p className='text-[1.5rem] w-[95%] m-auto text-center mb-[1rem]'>Our collaborators have more than 8+ years of experience, and the majority of them were featured on some of the biggest news portals in the USA.</p>
