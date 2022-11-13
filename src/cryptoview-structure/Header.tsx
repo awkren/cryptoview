43eabbef77ced56d18505da5a8553819e90895b5
Renan Araújo <renan.araujoonline@gmail.com>
2022-11-13 09:53:07 -0300

    top navbar is responsive

--- src/cryptoview-structure/Header.tsx
+++ src/cryptoview-structure/Header.tsx
@@ -1,4 +1,5 @@
 import React, {useState, useEffect} from 'react'
+import {FaTimes, FaBars} from 'react-icons/fa';
 import './Header.css'
 
 const Header = () => {
@@ -6,6 +7,9 @@ const Header = () => {
   const[toggleMenu, setToggleMenu] = useState(false);
   const [screenWidth, setScreenWidth] = useState(window.innerWidth);
   const toggleNav = () => {
+    // const cryptoLogoText = document.querySelector('.crypto-logo-text');
+    const cryptoLogoText = document.querySelectorAll<HTMLElement>('.crypto-logo-text');
+    // cryptoLogoText.hidden : true; 
     setToggleMenu(!toggleMenu);
   }
   useEffect(() => {
@@ -20,14 +24,21 @@ const Header = () => {
 
   return (
     <nav>
+        <p className='crypto-logo-text'>CryptoView</p>
         {(toggleMenu || screenWidth > 940) && (
             <ul className='list'>
-                <li className='items'>Home</li>
-                <li className='items'>Services</li>
-                <li className='items'>Contact</li>
+                <li className='items'>Earn</li>
+                <li className='items'>Courses</li>
+                <li className='crypto-logo-text-mobile'>CryptoView</li>
+                <li className='items'>Login</li>
+                <li className='items'>Sign Up</li>
             </ul>
+            
         )}
-        <button onClick={toggleNav} className='btn'>BTN</button>
+        {/* <button onClick={toggleNav} className='btn'>BTN</button> */}
+        <div className='btn' onClick={toggleNav}>
+                {toggleMenu ? (<FaTimes size={20} style={{color: '#FFF'}}/>) : (<FaBars size={20} style={{color: '#FFF'}}/>)}
+        </div>
     </nav>
   )
 }
