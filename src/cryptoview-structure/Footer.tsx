10611ffeda9a0e818d51c5240f29ab0c06320534
Renan Araújo <renan.araujoonline@gmail.com>
2022-11-13 11:13:31 -0300

    footer finished

--- src/cryptoview-structure/Footer.tsx
+++ src/cryptoview-structure/Footer.tsx
@@ -7,10 +7,10 @@ const Footer = () => {
     <section className='wrapper'>
       <div id='footer-main-div' className='w-[75%] text-[#FFFFFF] m-auto grid grid-cols-3 pt-[4rem] pb-[4rem]'>
         <div id='footer-left' className='flex flex-col gap-5'>
-          <p id='footer-title' className='text-[2rem] font-bold'>CryptoView</p>
-          <label id='label-join' className='text-[1.4rem] font-bold text-[#3629b0] mb-[-1rem] ml-[.5rem]'>Join our newsletter</label>
+          <p id='footer-title' className='text-[2.5rem] underline font-bold'>CryptoView</p>
+          <label id='label-join' className='text-[1.4rem] font-bold text-[white] mb-[-1rem] ml-[.5rem]'>Join our newsletter</label>
           <div id='mail-field' className='flex flex-row'>
-          <input type='email' className='w-[80%] h-[3rem] rounded border-4 border-[#3629b0]' id='newsletter-mail' placeholder='youremail@address.com' required></input>
+          <input type='email' className='w-[80%] h-[3rem] rounded border-4 text-[black] border-[white]' id='newsletter-mail' placeholder='youremail@address.com' required></input>
           <a href='/' id='sub-btn'>Subscribe</a>
           </div>
         </div>
