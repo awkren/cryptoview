172b8f7b48096a84d11055dd98ec6ff61dceb1f7
Renan Araújo <renan.araujoonline@gmail.com>
2022-11-18 17:34:51 -0300

    now we can register, login and get authorized with unique token

--- src/cryptoview-redir/AuthComponent.js
+++ src/cryptoview-redir/AuthComponent.js
@@ -1,9 +1,34 @@
-import React from 'react'
+import React, {useEffect, useState} from 'react'
+import axios from 'axios'
+import Cookies from 'universal-cookie'
 
 const AuthComponent = () => {
+  const cookies = new Cookies();
+  const token = cookies.get("TOKEN")
+  const [message, setMessage] = useState("")
+  useEffect(() => {
+    //set configs for api call
+    const configuration = {
+      method: 'get',
+      url: 'https://cryptoview-server-production.up.railway.app/auth-endpoint',
+      headers: {
+        Authorization: `Bearer ${token}`,
+      },
+    }
+    //make api call
+    axios(configuration)
+      .then((result) => {
+        //assign msg in our result to the msg we initialize above
+        setMessage(result.data.message)
+      })
+      .catch((error) => {
+        error = new Error()
+      })
+  }, [])
   return (
     <div>
       <h1 className='text-center'>Auth Component</h1>
+      <h3 className='text-center'>{message}</h3>
     </div>
   )
 }
--- src/cryptoview-redir/FreeComponent.js
+++ src/cryptoview-redir/FreeComponent.js
@@ -1,9 +1,29 @@
-import React from 'react'
+import React, {useEffect, useState} from 'react'
+import axios from 'axios'
 
 const FreeComponent = () => {
+  const [message, setMessage] = useState('');
+  useEffect(() => {
+    //set configs for api call
+    const configuration = {
+      method: 'get',
+      url: 'https://cryptoview-server-production.up.railway.app/free-endpoint',
+    }
+  //make api calls
+  axios(configuration)
+    .then((result) => {
+      //assign msg in result to the msg we initiliaze above
+      setMessage(result.data.message)
+    })
+    .catch((error) => {
+      error = new Error()
+    })
+  }, [])
   return (
     <div>
       <h1 className='text-center'>Free Component</h1>
+      {/* display msg from api call */}
+      <h3 className='text-center text-danger'>{message}</h3>
     </div>
   )
 }
