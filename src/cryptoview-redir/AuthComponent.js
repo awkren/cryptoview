7d93acc3fbb9c4d6ebed217e76a9f8104356bebf
Renan Araújo <renan.araujoonline@gmail.com>
2022-11-21 11:08:27 -0300

    connect api to get coins info

--- src/cryptoview-redir/AuthComponent.js
+++ src/cryptoview-redir/AuthComponent.js
@@ -1,13 +1,20 @@
 import React, {useEffect, useState} from 'react'
 import axios from 'axios'
 import Cookies from 'universal-cookie'
+import {FiArrowUp, FiArrowDown} from 'react-icons/fi'
 
 const AuthComponent = props => {
 
   const cookies = new Cookies();
   const token = cookies.get("TOKEN")
   const [message, setMessage] = useState("")
-  const [email, setEmail] = useState("")
+  const [data, setData] = useState(null)
+
+  const url = 'https://api.coingecko.com/api/v3/coins/markets?vs_currency=usd&order=market_cap_desc&per_page=6&page=1&sparkline=false'
+
+  
+  
+
   useEffect(() => {
     //set configs for api call
     const configuration = {
@@ -35,13 +42,28 @@ const AuthComponent = props => {
     //redirect user to home
     window.location.href= '/'
   }
+  
+  useEffect(() => {
+    axios
+      .get(url).then((response) => {
+        setData(response.data)
+      })
+      .catch((error) => {
+        console.log(error)
+      })
+  }, []);
 
-  return (
+  console.log(data)
+
+  if(!data) return null;
+
+  return (<>
     <div>
       <p className='text-[1rem] text-center mt-[.5rem] font-[400] text-[red] mb-[rem]'>You are logged in with this unique token:</p>
       <p className='text-center text-[green]'>{token}</p>
       <p className='text-[1rem] text-center mt-[rem] font-[400] mb-[.5rem] text-[red]'>Do not share it with anyone!</p>
       <button type='submit' className='block m-auto bg-[red] w-[5rem] h-[2.5rem] rounded-[6px] text-[white] mb-[.5rem]' onClick={() => logout()}variant='danger'>Logout</button>
+      {/* <p className='text-center italic'>powered by CoinGecko</p> */}
       <hr className='w-[70%] mb-[.5rem] m-auto'></hr>
       <p className='text-center text-[1.6rem] font-[300]'>Welcome to the</p>
       <p className='text-center text-[1.7rem] font-[500]'>CryptoView Coin Tracker</p>
@@ -49,7 +71,122 @@ const AuthComponent = props => {
       <p className='text-center italic text-[1.1rem]'>The best coin hub on the internet!</p>
       {/* logout button */}
     </div>
-  )
+
+    <div className='featured'>
+      <div id='coins-container' className=''>
+
+        {/* coins */}
+        <div id='coins' className='flex flex-row gap-[5rem] justify-center'>
+
+          <div className='card'>
+            <div className='top-card'>
+              <img src={data[0].image} alt='/'/>
+            </div>
+            <div>
+              <p>{data[0].name}</p>
+              <p>${data[0].current_price.toLocaleString()}  </p>
+            </div>
+            {data[0].price_change_percentage_24h < 0 ? (
+              <span className='red'>
+                <FiArrowDown className='icon'/>
+                {data[0].price_change_percentage_24h.toFixed(2)}%
+              </span>
+            ) : (
+              <span className='green'>
+                <FiArrowUp className='icon'/>
+                {data[0].price_change_percentage_24h.toFixed(2)}%
+              </span>
+            )}
+          </div>
+
+          <div className='card'>
+            <div className='top-card'>
+              <img src={data[1].image} alt='/'/>
+            </div>
+            <div>
+              <p>{data[1].name}</p>
+              <p>${data[1].current_price.toLocaleString()}  </p>
+            </div>
+            {data[1].price_change_percentage_24h < 0 ? (
+              <span className='red'>
+                <FiArrowDown className='icon'/>
+                {data[1].price_change_percentage_24h.toFixed(2)}%
+              </span>
+            ) : (
+              <span className='green'>
+                <FiArrowUp className='icon'/>
+                {data[1].price_change_percentage_24h.toFixed(2)}%
+              </span>
+            )}
+          </div>
+
+          <div className='card'>
+            <div className='top-card'>
+              <img src={data[2].image} alt='/'/>
+            </div>
+            <div>
+              <p>{data[2].name}</p>
+              <p>${data[2].current_price.toLocaleString()}  </p>
+            </div>
+            {data[2].price_change_percentage_24h < 0 ? (
+              <span className='red'>
+                <FiArrowDown className='icon'/>
+                {data[2].price_change_percentage_24h.toFixed(2)}%
+              </span>
+            ) : (
+              <span className='green'>
+                <FiArrowUp className='icon'/>
+                {data[2].price_change_percentage_24h.toFixed(2)}%
+              </span>
+            )}
+          </div>
+
+          <div className='card'>
+            <div className='top-card'>
+              <img src={data[3].image} alt='/'/>
+            </div>
+            <div>
+              <p>{data[3].name}</p>
+              <p>${data[3].current_price.toLocaleString()}  </p>
+            </div>
+            {data[3].price_change_percentage_24h < 0 ? (
+              <span className='red'>
+                <FiArrowDown className='icon'/>
+                {data[3].price_change_percentage_24h.toFixed(2)}%
+              </span>
+            ) : (
+              <span className='green'>
+                <FiArrowUp className='icon'/>
+                {data[3].price_change_percentage_24h.toFixed(2)}%
+              </span>
+            )}
+          </div>
+
+          <div className='card'>
+            <div className='top-card'>
+              <img src={data[4].image} alt='/'/>
+            </div>
+            <div>
+              <p>{data[4].name}</p>
+              <p>${data[4].current_price.toLocaleString()}  </p>
+            </div>
+            {data[4].price_change_percentage_24h < 0 ? (
+              <span className='red'>
+                <FiArrowDown className='icon'/>
+                {data[4].price_change_percentage_24h.toFixed(2)}%
+              </span>
+            ) : (
+              <span className='green'>
+                <FiArrowUp className='icon'/>
+                {data[4].price_change_percentage_24h.toFixed(2)}%
+              </span>
+            )}
+          </div>
+
+        </div>
+      </div>
+    </div>
+  </>)
 }
 
 export default AuthComponent
