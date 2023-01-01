bec5ebf51f385c8a3b907e197396091032732f7c
Renan Araújo <renan.araujoonline@gmail.com>
2023-01-01 13:05:29 -0300

    fixed experts section text overlay on 1360px

--- src/cryptoview-structure/AboutExperts.tsx
+++ src/cryptoview-structure/AboutExperts.tsx
@@ -12,9 +12,11 @@ const AboutExperts = () => {
       </div>
       <div id='about-experts-right' className='m-auto text-center'>
         <p id='title-experts' className='text-[3.5rem] font-bold'>We work with the best</p>
-        <p className='text-[1.5rem] w-[95%] m-auto text-justify mt-[1rem] mb-[1rem]'>Our consultors have worked in the biggest banks in the world.
-        We understand what brings results, that's why we are willing to give you the best experience and professionals in the market.</p>
-        <p className='text-[1.5rem] w-[95%] m-auto text-center mb-[1rem]'>Our collaborators have more than 8+ years of experience, and the majority of them were featured on some of the biggest news portals in the USA.</p>
+        <div className='on-experts'>
+          <p className='text-[1.5rem] w-[95%] m-auto text-justify mt-[1rem] mb-[1rem]'>Our consultors have worked in the biggest banks in the world.
+          We understand what brings results, that's why we are willing to give you the best experience and professionals in the market.</p>
+          <p className='text-[1.5rem] w-[95%] m-auto text-center mb-[1rem]'>Our collaborators have more than 8+ years of experience, and the majority of them were featured on some of the biggest news portals in the USA.</p>
+        </div>
         <p id='news' className='flex gap-[3rem] text-[3.2rem] justify-center mb-[1rem]'><SiCnn title='CNN'/><SiBbc title='BBC'/><SiThewashingtonpost title='The Washington Post'/><SiNewyorktimes title='The New York Times'/></p>
         <a href='/readmore' className='text-[1.5rem] font-semibold text-[#3629b0] flex items-center justify-center'><TiArrowForward className='mr-[.3rem]'/>Read more about them!</a>
       </div>
